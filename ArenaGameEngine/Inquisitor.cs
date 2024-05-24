e62338a2faa8df6d29a13f3d3480482c18c73172
Iliyan Farfalov <166518554+Iliyan04@users.noreply.github.com>
2024-05-24 19:46:39 +0300

    Update Inquisitor.cs

--- ArenaGameEngine/Inquisitor.cs
+++ ArenaGameEngine/Inquisitor.cs
@@ -8,35 +8,35 @@ namespace ArenaGameEngine
 {
     public class Inquisitor : Hero
     {
-        // Допълнителни характеристики за инквизитора
+        
         public bool DivineProtection { get; private set; }
 
-        // Конструктор за инквизитора
+       
         public Inquisitor(string name) : base(name)
         {
             DivineProtection = false;
         }
 
-        // Презаписване на метода за атака
+     
         public override int Attack()
         {
-            // В инквизитора атаката може да активира допълнителен ефект, ако е включена божествена защита
+            
             if (DivineProtection)
             {
-                // Логика за божествена защита
+                
                 return base.Attack() + 20;
             }
             else
             {
-                // Ако божествената защита не е активирана, атаката се извършва само с базовата си сила
+              
                 return base.Attack();
             }
         }
 
-        // Презаписване на метода за получаване на щети
+   
         public override void TakeDamage(int incomingDamage)
         {
-            // В инквизитора може да се добави способност за активиране на божествена защита при определени условия
+          
             if (incomingDamage > 200)
             {
                 DivineProtection = true;
